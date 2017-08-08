3229fc8a3e00e14b7232f6ea3afb5f30f1da101e
eriollee <eriollee@163.com>
2017-08-08 22:06:47 +0800

    使用响应式表单构建模型

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ChildComponent } from './child/child.component';
 import { Child2Component } from './child2/child2.component';
 import { TemplateFormComponent } from './template-form/template-form.component';
 import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
+import { ReactiveRegistComponent } from './reactive-regist/reactive-regist.component';
 
 var routerConfig: Routes =[
   {path:'',component:ChildComponent},
@@ -31,7 +32,8 @@ var routerConfig: Routes =[
     ChildComponent,
     Child2Component,
     TemplateFormComponent,
-    ReactiveFormComponent
+    ReactiveFormComponent,
+    ReactiveRegistComponent
   ],
   imports: [
     BrowserModule,
